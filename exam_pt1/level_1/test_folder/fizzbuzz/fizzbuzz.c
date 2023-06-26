013a232201a177f88b98c7ddbe9301e507851288
Tania Boterman <taboterm@c1s12d57.42wolfsburg.de>
2023-06-26 15:56:46 +0200

    new

--- exam_pt1/level_1/test_folder/fizzbuzz/fizzbuzz.c
+++ exam_pt1/level_1/test_folder/fizzbuzz/fizzbuzz.c
@@ -1,33 +1,28 @@
 #include <unistd.h>
 
-void ft_putnum(int n)
+void	ft_putnum(int n)
 {
-	char str[10] = "0123456789";
-	if (n > 9)
+	if(n >= 10)
 		ft_putnum(n / 10);
-	write(1, &str[n % 10], 1);
+	char c = (n % 10) + '0';
+	write(1, &c, 1);
 }
 
-int	main(int ac, char **av)
+int	main()
 {
-	int i = 0;
-
-	if (ac == 1)
+	int n = 1;
+	while(n <= 100)
 	{
-		while(i <= 100)
-		{
-			if(i % 15 == 0)
-				write(1, "fizzbuzz", 8);
-			else if(i % 3 == 0)
-				write(1, "fizz", 4);
-			else if(i % 4 == 0)
-				write(1, "buzz", 4);
-			else
-				ft_putnum(i);
-			i++;
-			write(1, "\n", 1);
-		}
+		if (n % 15 == 0)
+			write(1, "fizzbuzz", 8);
+		else if(n % 3 == 0)
+			write(1, "fizz", 4);
+		else if(n % 5 == 0)
+			write(1, "buzz", 4);
+		else
+			ft_putnum(n);
+		write(1, "\n", 1);
+		n++;
 	}
-	write(1, "\n", 1);
 	return 0;
 }
